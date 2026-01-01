5bd9bbb4effb4867a06f5696638456bd5810bc3b
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2026-01-01 16:42:54 +0100

    diff2html 3.4.53

--- inst/prepare_lib.R
+++ inst/prepare_lib.R
@@ -7,7 +7,7 @@ unlink("inst/htmlwidgets/lib", recursive = TRUE)
 dir.create("inst/htmlwidgets/lib")
 
 highlightjs_ver <- "11.11.1"
-diff2html_ver <- "3.4.52"
+diff2html_ver <- "3.4.53"
 jsdiff_ver <- "8.0.2"
 
 
